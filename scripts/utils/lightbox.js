8ae5efac53f4eb3deced2a9f4779c0c0b8f1f5e8
Fablaney <fabrmoto@gmail.com>
2022-06-22 17:35:18 +0200

    a

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,5 +1,8 @@
 function openLightBox()
 {
     document.querySelector(".lightbox-container").classList.remove("d-none")
-    
+}
+function closeLightBox()
+{
+    document.querySelector(".lightbox-container").classList.add("d-none")
 }
