dd7e9e11460c04bea8ff113d7ace355e8819bf89
Fablaney <fabrmoto@gmail.com>
2022-06-29 11:15:55 +0200

    finitions

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -3,6 +3,9 @@ function displayModal()
     // J'enleve le d-none sur la modale
     document.querySelector(".contact_modal").classList.remove("d-none")
 
+    document.querySelector('.header').setAttribute('aria-hidden', 'true')
+    document.querySelector('#main').setAttribute('aria-hidden', 'true')
+
     // nom du photographe
     let name = onePhotographer.name
 
@@ -20,25 +23,24 @@ function displayModal()
 
                         <div>
                             <label for="prenom">Prénom</label>
-                            <input name="prenom" id="prenom" type="text" required/>
+                            <input name="prenom" id="prenom" type="text" required aria-label="First Name"/>
                         </div>
 
                         <div>
                             <label for="nom">Nom</label>
-                            <input name="nom" id="nom" type="text" required/>
+                            <input name="nom" id="nom" type="text" required aria-label="Last Name"/>
                         </div>
 
                         <div>
                             <label for="email">Email</label>
-                            <input name="email" id="email" type="email" required/>
-                        </div>
+                            <input name="email" id="email" type="email" required aria-label="Email">
 
                         <div>
                             <label for="message">Votre message</label>
-                            <textarea name="message" id="message" type="text" required></textarea>
+                            <textarea name="message" id="message" type="text" required aria-label="Your message"></textarea>
                         </div>
 
-                        <button class="contact_button" onclick="validateModal()">Envoyer</button>
+                        <button class="contact_button" onclick="validateModal()" aria-label="Send">Envoyer</button>
 
                     </form>
 
@@ -49,7 +51,7 @@ function displayModal()
 
     // j'insere le bloc html  du formulaire dans la modale
     document.querySelector(".contact_modal").innerHTML = modalDOM;
-
+ 
 
     let form = document.getElementById("form-modal")
 
@@ -58,25 +60,62 @@ function displayModal()
     { 
         e.preventDefault()
     })
+
+
+
+    function listener()
+    {
+        document.querySelector(".lightbox__prev").addEventListener("click", () => {
+            prev()
+        })
+
+        document.querySelector(".lightbox__next").addEventListener("click", () => {
+            next()
+        })
+
+        document.addEventListener('keyup', (el) =>{
+            switch(el.key)
+            {
+                case "ArrowLeft" :
+                    prev(); 
+                    break; 
+                case "ArrowRight": 
+                    next();
+                    break; 
+                case "Escape" : 
+                    closeModal()
+                    document.querySelectorAll("body *:not(dialog)").removeAttribue('aria-hidden')
+                    break; 
+            }
+            console.log(el.key)
+        })
+    }
+    listener()
 }
 
+let formNom
+let formPrenom
+let formEmail
+let formMessage
 
 function validateModal()
 {
+    formNom = document.querySelector("#nom").value
+    formPrenom = document.querySelector("#prenom").value
+    formEmail = document.querySelector("#email").value
+    formMessage = document.querySelector("#message").value
+
     // Je récupere la valeur du champ et l'affiche en console
-    let formNom = document.querySelector("#nom").value
-    console.log("Nom : " + formNom)
-    
-    let formPrenom = document.querySelector("#prenom").value
-    console.log("Prénom : " + formPrenom)
+    if ( formNom.length > 0 && formPrenom.length > 0 && formEmail.length > 0 && formMessage.length > 0 )
+    {
+        console.log("Nom : " + formNom)
+        console.log("Prénom : " + formPrenom)
+        console.log("Email : " + formEmail)
+        console.log("Message: " + formMessage)
 
-    let formEmail = document.querySelector("#email").value
-    console.log("Email : " + formEmail)
-
-    let formMessage = document.querySelector("#message").value
-    console.log("Message: " + formMessage)
-
-    closeModal()
+        closeModal()
+     
+    }
 } 
 
 
@@ -84,4 +123,5 @@ function validateModal()
 function closeModal()
 {
     document.querySelector(".contact_modal").classList.add("d-none")
+
 }
